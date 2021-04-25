d9c7a02a5080e5e9d3cbcf19602e46f8228579b6
Iurii <juravyn@gmail.com>
2021-04-25 21:15:20 +0300

    Added clockk and inputDate

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 
 const todoForm = document.querySelector(".todo__form");
 const todoDesc = todoForm.querySelector(".todo__description");
+const todoTime = todoForm.querySelector(".todo__duedate");
 const todoWrapper = document.querySelector(".todo__wrapper");
 
-let filterIndicator = 0; 
+let filterId = 0; 
 let tasksArray;
 
 if (localStorage.tasks) {
@@ -21,28 +22,23 @@ function fillToDoList () {
              todoWrapper.innerHTML += createTask (task, index);
         });
     }
-    addClick ();
+    addClickToTaskItem ();
 }
 
-function addClick () {
+fillToDoList ();
+
+function addClickToTaskItem () {
     const todoTask = document.querySelectorAll(".todo__item");
     
     todoTask.forEach(task => {
         task.addEventListener ("click", (e) => {
             if (e.target.classList.contains("todo__complete")) {
-                let i = e.target.dataset.complete;  
                 
-                if (!tasksArray[i].completed) {
-                    tasksArray[i].completed = true;
-                    tasksArray[i].active = false;
-                    updateLocalStorage();
-                    fillToDoList ();
+                if (!tasksArray[e.target.dataset.complete].completed) {
+                    changeCompletedStatus (e.target.dataset.complete);
                 }
                 else {
-                    tasksArray[i].completed = false;
-                    tasksArray[i].active = true;
-                    updateLocalStorage();
-                    fillToDoList ();
+                    changeCompletedStatus (e.target.dataset.complete);
                 }
             }
 
@@ -50,26 +46,36 @@ function addClick () {
                 let i = e.target.dataset.delete;
                 
                 if (!tasksArray[i].deleted) {
-                    tasksArray[i].deleted = true;
-                    tasksArray[i].active = false;
-                    updateLocalStorage();
-                    fillToDoList ();
+                    tasksArray[i].deleted = !tasksArray[i].deleted;
+                    tasksArray[i].active = !tasksArray[i].active;
+                    updateLocalStorageAndToDoList ();
                 }
             }
         });
     });
-    filterTasks (filterIndicator);
+    filterTasks (filterId);
 }
 
-fillToDoList ();
+function changeCompletedStatus (i) {
+    tasksArray[i].completed = !tasksArray[i].completed;
+    tasksArray[i].active = !tasksArray[i].active;
+    updateLocalStorageAndToDoList ();
+}
+
+function updateLocalStorageAndToDoList () {
+    updateLocalStorage();
+    fillToDoList ();
+}
+
 
 function updateLocalStorage () {
     localStorage.setItem("tasks", JSON.stringify(tasksArray));
 }
 
 class newToDoTask {
-    constructor(desc, active, completed, deleted){
+    constructor(desc, deadline, active, completed, deleted){
         this.desc = desc;
+        this.deadline = deadline;
         this.active = active;
         this.completed = completed;
         this.deleted = deleted;
@@ -79,7 +85,7 @@ class newToDoTask {
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    tasksArray.push(new newToDoTask (todoDesc.value, true ,false, false));
+    tasksArray.push(new newToDoTask (todoDesc.value, todoTime.value, true ,false, false));
     updateLocalStorage ();
     fillToDoList ();
     todoForm.reset();
@@ -93,11 +99,13 @@ function createTask (task, index) {
     ${task.deleted ? "deleted" : ''}" 
     data-task="${index}">    
         <div class="todo__general">
-            <div class="todo__text">${task.desc}</div>
-            <div class="todo__deadline">Date</div>
-            <div class="todo__show"><img src="img/nav/down-arrow.png" alt="down_arrow"></div>
-            <input type="checkbox" class="todo__complete" data-complete="${index}" ${task.completed ? "checked": ""}>
-            <div class="todo__delete" data-delete="${index}">&#10060</div>
+                <div class="todo__text">${task.desc}</div>
+            <div class="todo__options">
+                <div class="todo__deadline">${task.deadline ? task.deadline : ""}</div>
+                <div class="todo__show"><img src="img/nav/down-arrow.png" alt="down_arrow"></div>
+                <input type="checkbox" class="todo__complete" data-complete="${index}" ${task.completed ? "checked": ""}>
+                <div class="todo__delete" data-delete="${index}">&#10060</div>
+            </div>
         </div>
         <div class="todo__additional">
 
@@ -106,70 +114,116 @@ function createTask (task, index) {
     `;
 }
 
-function addNonActiveButton () {
+function changeActiveButton () {
     document.querySelectorAll(".filter div").forEach(btn => {
         btn.classList.add("non-active-button");
         btn.classList.remove("active-button");
     });
-    document.querySelectorAll(".filter div")[filterIndicator].classList.add("active-button");
+    document.querySelectorAll(".filter div")[filterId].classList.add("active-button");
 }
 
-addNonActiveButton ();
+changeActiveButton ();
 
-const filterTaskBtn = document.querySelector(".filter");
 
-filterTaskBtn.addEventListener("click", (e) => {
+document.querySelector(".filter").addEventListener("click", (e) => {
     
     if (e.target.classList.contains("act")) {
-        filterIndicator = 0;
-        e.target.classList.add("active-button");
-        addNonActiveButton (filterIndicator);
+        filterId = 0;
     }
     else if (e.target.classList.contains("del")) {
-        filterIndicator = 1;
-        e.target.classList.add("active-button");
-        addNonActiveButton (filterIndicator);
+        filterId = 1;
     }
     else if (e.target.classList.contains("com")) {
-        filterIndicator = 2;
-        e.target.classList.add("active-button");
-        addNonActiveButton (filterIndicator);
+        filterId = 2;
     }
     else if (e.target.classList.contains("all")) {
-        filterIndicator = 3;
-        e.target.classList.add("active-button");
-        addNonActiveButton (filterIndicator);
+        filterId = 3;
     }
-    filterTasks (filterIndicator);
+
+    changeActiveButton (filterId);
+    filterTasks (filterId);
 });
 
-function filterTasks(i) {
+function filterTasks(id) {
     const tasksList = document.querySelectorAll(".todo__item");
     
+    hideFilteredTasks (tasksList);
+    
+    tasksList.forEach(tasks => {
+        
+        if (id == 0 && tasks.classList.contains("active") && !tasks.classList.contains("deleted")) {
+            showFilteredTask (tasks);            
+        }
+        else if (id == 1 && tasks.classList.contains("deleted")) {
+            showFilteredTask (tasks);
+        }
+        else if (id == 2 && tasks.classList.contains("checked") && !tasks.classList.contains("deleted")) {
+            showFilteredTask (tasks);
+        }
+        else if (id == 3) {
+            showFilteredTask (tasks);
+        }
+
+    });
+}
+
+function hideFilteredTasks (tasksList) {
     tasksList.forEach(task => {
         task.classList.remove("show");
         task.classList.add("hide");
     });
-    
-    tasksList.forEach(task => {
-        
-        if (i == 0 && task.classList.contains("active") && !task.classList.contains("deleted")) {
-            changeVisibility (task);            
-        }
-        else if (i == 1 && task.classList.contains("deleted")) {
-            changeVisibility (task);
-        }
-        else if (i == 2 && task.classList.contains("checked") && !task.classList.contains("deleted")) {
-            changeVisibility (task);
-        }
-        else if (i == 3) {
-            changeVisibility (task);
-        }
-
-    });
 }
 
-function changeVisibility (task) {
+function showFilteredTask (task) {
     task.classList.remove("hide");
     task.classList.add("show");
 }
+
+
+function getDate (currentDate) {
+    const date = getFirstZero(currentDate.getDate());
+    const year = currentDate.getFullYear();
+    const month = getMonthName(currentDate.getMonth());
+    const day = getDayName(currentDate.getDay());
+
+    return {year, date, month, day};
+}
+
+function getMonthName (monthNumber) {
+    const monthList = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOW", "DEC"];
+    return monthList[monthNumber];
+}
+
+function getDayName (dayNumber) {
+    const dayList = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+    return dayList[dayNumber];
+}
+
+function getTime (currentDate) {
+    const hours = getFirstZero(currentDate.getHours());
+    const minutes = getFirstZero(currentDate.getMinutes());
+    const seconds = getFirstZero(currentDate.getSeconds());
+    
+    return {hours, minutes, seconds};
+}
+function setClock () {
+    const clock = document.querySelector(".header__clock");
+    const d = getDate(new Date());
+    const t = getTime(new Date());
+    
+
+    clock.innerHTML = `
+        ${d.date}-(${d.day})-${d.month}-${d.year}   ${t.hours}:${t.minutes}:${t.seconds}`;
+}
+
+function getFirstZero (num) {
+    if (num < 10) {
+        num = "0" + num;
+    }
+    return num;
+}
+
+setClock ();
+setInterval(setClock, 1000);
+
+
